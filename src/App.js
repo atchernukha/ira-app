7a7afb864bebe52c5c5ad7a6c8598d3f6c0879a8
Anthon Tchernukha <toha77747@gmail.com>
2021-05-02 04:46:10 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
+import Main from "./components/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <Header className="header"></Header>
+      <div className="main">
+      <Sidebar className="sidebar"></Sidebar>
+      <Main ></Main>
+      </div>
     </div>
   );
 }
